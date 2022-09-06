e089bdb8c19bb20b6a03ed44db1943c436170793
virtu-alex <alessio.cordari@gmail.com>
2022-09-06 14:04:14 +0200

    6th (SELECT)

--- db-sql/select.sql
+++ db-sql/select.sql
@@ -36,11 +36,13 @@ SELECT *
 FROM `degrees`
 WHERE `level` = 'magistrale';
 
-
 --7. Da quanti dipartimenti è composta l'università? (12)
 
 
 
 --8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+
+
+
 
 
