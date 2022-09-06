4011c7e44ad4843d952b76520c97a4d188630ffd
virtu-alex <alessio.cordari@gmail.com>
2022-09-06 13:51:15 +0200

    4th (SELECT)

--- db-sql/select.sql
+++ db-sql/select.sql
@@ -1,12 +1,12 @@
 --1. Selezionare tutti gli studenti nati nel 1990 (160)
 
-SELECT `date_of_birth`
+SELECT *
 FROM `students`
 WHERE `date_of_birth` LIKE "1990-%-%"
 
 --2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
-SELECT `cfu`
+SELECT *
 FROM `courses`
 WHERE `cfu` > 10
 
@@ -16,11 +16,12 @@ SELECT *
 FROM `students`
 WHERE `date_of_birth` < '1992-%-%';
 
-
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-
-
+SELECT * 
+FROM `courses`
+WHERE `period` = 'I semestre'
+AND `year` = 1;
 
 --5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
