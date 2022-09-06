7a8d1d8c8c06798ffb14741e866553677a2fca14
virtu-alex <alessio.cordari@gmail.com>
2022-09-06 14:52:20 +0200

    4th (GROUP BY)

--- db-sql/groupby.sql
+++ db-sql/groupby.sql
@@ -6,16 +6,18 @@ GROUP BY `anno`;
 
 --2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
-SELECT COUNT(*) as 'insegnanti' , `office_address` AS 'locazione'
+SELECT COUNT(*) AS 'insegnanti' , `office_address` AS 'locazione'
 FROM `teachers`
 GROUP BY `office_address`;
 
 --3. Calcolare la media dei voti di ogni appello d'esame
 
-SELECT AVG(`vote`) , `exam_id`
+SELECT AVG(`vote`) AS `media_voto` , `exam_id` AS `appelli`
 FROM `exam_student`
-GROUP BY `exam_id`;
+GROUP BY `appelli`;
 
 --4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
-
+SELECT COUNT(`id`), `department_id` AS `dipartimenti`
+FROM `degrees`
+GROUP BY `dipartimenti`;
