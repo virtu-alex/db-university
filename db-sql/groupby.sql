ef26c009edf4ef9265c831f0e7643d3eef99f9c5
virtu-alex <alessio.cordari@gmail.com>
2022-09-06 14:48:38 +0200

    fixed 3rd (GROUP BY)

--- db-sql/groupby.sql
+++ db-sql/groupby.sql
@@ -12,7 +12,9 @@ GROUP BY `office_address`;
 
 --3. Calcolare la media dei voti di ogni appello d'esame
 
-
+SELECT AVG(`vote`) , `exam_id`
+FROM `exam_student`
+GROUP BY `exam_id`;
 
 --4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
