0988952b26b4d332fbf1ce269bb0bd57e23805f4
virtu-alex <alessio.cordari@gmail.com>
2022-09-07 00:07:44 +0200

    fix ex 3 select

--- db-sql/select.sql
+++ db-sql/select.sql
@@ -12,9 +12,9 @@ WHERE `cfu` > 10
 
 --3. Selezionare tutti gli studenti che hanno più di 30 anni
 
-SELECT *
-FROM `students`
-WHERE `date_of_birth` < '1992-%-%';
+SELECT * 
+FROM `students` 
+WHERE YEAR(`date_of_birth`) < (YEAR(CURDATE()) - 30);
 
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
