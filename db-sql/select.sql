f2850b2490a6c6a3a89a267899b2f071ff060ace
virtu-alex <alessio.cordari@gmail.com>
2022-09-06 13:38:01 +0200

    2nd (SELECT)

--- db-sql/select.sql
+++ db-sql/select.sql
@@ -6,6 +6,9 @@ WHERE `date_of_birth` LIKE "1990-%-%"
 
 --2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
+SELECT `cfu`
+FROM `courses`
+WHERE `cfu` > 10
 
 
 --3. Selezionare tutti gli studenti che hanno più di 30 anni
