d62fe7981ffb6f3907974c426783489dad82fc22
virtu-alex <alessio.cordari@gmail.com>
2022-09-06 13:37:36 +0200

    1st (SELECT)

--- db-sql/select.sql
+++ db-sql/select.sql
@@ -1,6 +1,8 @@
 --1. Selezionare tutti gli studenti nati nel 1990 (160)
 
-
+SELECT `date_of_birth`
+FROM `students`
+WHERE `date_of_birth` LIKE "1990-%-%"
 
 --2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
