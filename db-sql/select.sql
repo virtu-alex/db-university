e2a09d481b02bf03c73b7ae6716f0e8117a6fa68
virtu-alex <alessio.cordari@gmail.com>
2022-09-06 13:45:11 +0200

    3rd (SELECT)

--- db-sql/select.sql
+++ db-sql/select.sql
@@ -10,12 +10,15 @@ SELECT `cfu`
 FROM `courses`
 WHERE `cfu` > 10
 
-
 --3. Selezionare tutti gli studenti che hanno più di 30 anni
 
+SELECT *
+FROM `students`
+WHERE `date_of_birth` < '1992-%-%';
 
 
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+
 
 
 
